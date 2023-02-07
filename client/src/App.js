0bb03fa1eb480a541b718af26fbac3e06c722f34
Harnaljia <harnaljia.fenelon@tufts.edu>
2023-02-06 21:17:53 -0500

    Small page changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,3 @@
-import Header from './components/Header.js';
-import PatientInfo from './components/PatientInfo.js';
-import KeyFindings from './components/KeyFindings.js';
-import AllDataButton from './components/AllDataButton.js';
-import Image from './components/Image.js';
 import './App.css';
 import Header from './components/Header.js';
 import PatientInfo from './components/PatientInfo.js';
@@ -16,32 +11,6 @@ import { API_BASE } from "./constants";
 import { useApi } from './hooks/use-api';
 
 function App() {
-<<<<<<< Updated upstream
-  const [data, setData] = useState("")
-  // const { response } = useApi();
-
-  useEffect(() => {
-    console.log("call api")
-    fetch("http://localhost:8000/api")
-      .then(res => res.json())
-      .then(data1 => {
-        setData(data1)
-      })
-      .catch(err => console.log(err))
-
-  }, [])
-
-
-
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          {data}
-        </p>
-      </header>
-=======
   return (
     <div className="App">
       <Header />
@@ -55,10 +24,8 @@ function App() {
           <PatientInfo />
         </div>
       </div>
->>>>>>> Stashed changes
     </div>
   );
 }
-
 
 export default App;
--- client/src/components/KeyFindings.js
+++ client/src/components/KeyFindings.js
@@ -1,11 +1,8 @@
 const KeyFindings = () => {
     return (
         <div className="InfoBox">
-<<<<<<< Updated upstream
-            <p> Significant worsening of airspace disease, now very extensive and patchy sparing only apices. </p>
-=======
-            <p> Significant worsening of airspjjnhbjhbhjhgjhb b jhbhhbhhace disease, now very extensive and patchy sparing only apices. </p>
->>>>>>> Stashed changes
+            <h2> Key Findings: </h2>
+            <p> Significant worsening of disease, now very extensive and patchy sparing only apices. </p>
         </div>
     );
 }
