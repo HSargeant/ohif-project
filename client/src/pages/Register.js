85bfe1bad9ba8bafaf9effef0f9d05002e3c90c7
hsargeant <hssoftwareengineering@gmail.com>
2023-02-13 12:27:57 -0500

    register account page + mui install

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,177 @@
-//sign up page
+import { useNavigate, useOutletContext } from "react-router-dom";
+import {useState} from "react"
 
-export default function Signup(){
-    return(
-        <h1>signup page</h1>
-    )
+import {
+	Box,
+	Button,
+	Container,
+	Link,
+	TextField,
+	Typography
+  } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import GoogleIcon from '@mui/icons-material/Google';
+
+export default function Signup() {
+	const { setUser,messages, setMessages,user } = useOutletContext();
+  const [errorMsg,setErrorMsg] = useState("")
+	const navigate = useNavigate();
+
+	if(user) {
+		 navigate("/profile")
+		 return
+	}
+
+	const handleSubmit = async (event) => {
+	// 	event.preventDefault();
+	// 	const form = event.currentTarget;
+    // // if(event.target[4].value!==event.target[6].value){
+    // //   window.alert("Passwords Do Not Match")
+    // //   return
+    // // }
+	// 	const response = await fetch(API_BASE + form.getAttribute('action'), {
+	// 		method: form.method,
+	// 		body: new URLSearchParams(new FormData(form)),
+	// 		credentials: "include"
+	// 	});
+	// 	const data = await response.json();
+	// 	if (data.messages){ 
+    //   setMessages(data.messages)
+    //   setErrorMsg(data.messages.errors[0].msg)
+  
+    // }
+	// 	if (data.user) {
+	// 		setUser(data.user);
+	// 		navigate("/profile");
+	// 	}
+	};
+
+//   console.log(errorMsg)
+	return (
+		<Box
+        component="main"
+        sx={{
+          alignItems: 'center',
+          display: 'flex',
+          flexGrow: 1,
+          minHeight: '100%'
+        }}
+      >
+        <Container maxWidth="sm">
+          <form action="/signup" method="POST" onSubmit={handleSubmit}>
+            <Box sx={{ my: 3 }}>
+              <Typography
+                color="textPrimary"
+                variant="h4"
+              >
+                Create a new account
+              </Typography>
+              <Typography
+                color="textSecondary"
+                gutterBottom
+                variant="body2"
+              >
+                Use your email to create a new account
+              </Typography>
+			  
+            </Box>
+            <TextField
+            required={true}
+              fullWidth
+              label="First Name"
+			  id="firstName"
+              margin="normal"
+              name="firstName"
+              variant="outlined"
+			  type="text"
+            />
+            <TextField
+            required={true}
+              fullWidth
+              label="Last Name"
+			  id="lastName"
+              margin="normal"
+              name="lastName"
+              variant="outlined"
+			  type="text"
+            />
+            <TextField
+            required={true}
+              fullWidth
+              label="Email Address"
+              margin="normal"
+              name="email"
+              type="email"
+			  id="exampleInputEmail1"
+              variant="outlined"
+			  aria-describedby="emailHelp"
+            />
+			<Typography
+                color="textSecondary"
+                gutterBottom
+                variant="body2"
+              >
+                We'll never share your email address.
+              </Typography>
+            <TextField
+            required={true}
+              fullWidth
+              label="Password"
+              margin="normal"
+              name="password"
+              type="password"
+			  id="password"
+              variant="outlined"
+            />
+            <TextField
+            required={true}
+              fullWidth
+              label="Confirm Password"
+              margin="normal"
+			  id="confirmPassword"
+              name="confirmPassword"
+              type="password"
+              variant="outlined"
+            />
+            <Box
+              sx={{
+                alignItems: 'center',
+                display: 'flex',
+                ml: -1
+              }}
+            >
+
+              
+            </Box>
+            {errorMsg ? <div> {errorMsg}</div>:""}
+            <Box sx={{ py: 2 }}>
+              <Button
+                color="primary"
+                fullWidth
+                size="large"
+                type="submit"
+                variant="contained"
+              >
+                Sign Up Now
+              </Button>
+            </Box>
+            <Typography
+              color="textSecondary"
+              variant="body2"
+            >
+              Have an account?
+              {' '}
+         
+                <Link
+                href="/"
+                  variant="subtitle2"
+                  underline="hover"
+                >
+                  Sign In
+                </Link>
+            </Typography>
+          </form>
+        </Container>
+      </Box>
+	);
 }
