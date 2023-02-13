dbbd3235439d79b0cb4c53b822b3314d5e3b1de3
hsargeant <hssoftwareengineering@gmail.com>
2023-02-13 12:30:42 -0500

    add login page temp style

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,12 +1,153 @@
-// login page
+import { useNavigate, useOutletContext } from "react-router-dom";
+import { API_BASE } from "../constants";
 
-export default function Login(){
-    return(
+import { Box, Button, Container, Grid, TextField, Typography,Link } from '@mui/material';
+import GoogleIcon from '@mui/icons-material/Google';
 
-        <>
-        <h1>login page</h1>
-        dfsgsdfgsdfgsdb sdg
-        </>
-        
-    )
+export default function Login() {
+  const { user,setUser, setMessages } = useOutletContext();
+	const navigate = useNavigate();
+	if(user) {
+		 navigate("/profile")
+		 return
+	}
+	const handleSubmit = async (event) => {
+		// event.preventDefault();
+		// const form = event.currentTarget;
+		// const response = await fetch(API_BASE + form.getAttribute('action'), {
+		// 	method: form.method,
+		// 	body: new URLSearchParams(new FormData(form)),
+		// 	credentials: "include"
+		// });
+		// const data = await response.json();
+		// if (data.messages) setMessages(data.messages);
+		// if (data.user) {
+		// 	setUser(data.user);
+		// 	navigate("/profile");
+		// }
+	};
+	return (
+      <Box
+        component="main"
+        sx={{
+          alignItems: 'center',
+          display: 'flex',
+          flexGrow: 1,
+          minHeight: '100%'
+        }}
+      >
+        <Container maxWidth="sm">
+          <form action="/login" method="POST" onSubmit={handleSubmit}>
+            <Box sx={{ my: 3 }}>
+              <Typography
+                color="textPrimary"
+                variant="h4"
+              >
+                Welcome<br/>
+              </Typography>
+              <Typography
+                color="textSecondary"
+                gutterBottom
+                variant="body2"
+              >
+              </Typography>
+            </Box>
+            {/* <Grid
+              container
+              spacing={3}
+            > */}
+   
+              <Grid
+                item
+                xs={12}
+                md={6}
+              >
+                <Button
+                  color="error"
+                  fullWidth
+                  // onClick=""
+                  size="large"
+                  startIcon={<GoogleIcon />}
+                  variant="contained"
+                >
+                  Login with Google
+                </Button>
+              </Grid>
+            {/* </Grid> */}
+            <Box
+              sx={{
+                pb: 1,
+                pt: 3
+              }}
+            >
+              <Typography
+                align="center"
+                color="textSecondary"
+                variant="body1"
+              >
+                or login with email address
+              </Typography>
+            </Box>
+            <TextField
+            //   error={Boolean(formik.touched.email && formik.errors.email)}
+              fullWidth
+            //   helperText={formik.touched.email && formik.errors.email}
+              label="Email Address"
+              margin="normal"
+              name="email"
+            //   onBlur={formik.handleBlur}
+            //   onChange={formik.handleChange}
+              type="email"
+            //   value={formik.values.email}
+              variant="outlined"
+              className="form-label"
+            />
+            <TextField
+              fullWidth
+              label="Password"
+              margin="normal"
+              name="password"
+              type="password"
+              variant="outlined"
+            />
+            <Box sx={{ py: 2 }}>
+              <Button
+                color="primary"
+                fullWidth
+                size="large"
+                type="submit"
+                variant="contained"
+              >
+                Sign In
+              </Button>
+            </Box>
+            <Typography
+              color="textSecondary"
+              variant="body2"
+            >
+              Don&apos;t have an account?
+              {' '}
+                <Link href="/signup"
+                     variant="subtitle2"
+                     underline="hover"
+                     sx={{
+                       cursor: 'pointer'
+                     }}
+                >
+                {/* <Link
+                  variant="subtitle2"
+                  underline="hover"
+                  sx={{
+                    cursor: 'pointer'
+                  }}
+                > */}
+                  Sign Up
+                {/* </Link> */}
+                </Link>
+              
+            </Typography>
+          </form>
+        </Container>
+      </Box>
+	);
 }
