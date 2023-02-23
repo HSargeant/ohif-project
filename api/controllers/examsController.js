920be50c03f8c9e77a1d17f3bec741cbeedad3bc
hsargeant <hssoftwareengineering@gmail.com>
2023-02-23 12:56:07 -0500

    add delete button and functionality

--- api/controllers/examsController.js
+++ api/controllers/examsController.js
@@ -73,8 +73,6 @@ module.exports = {
     }
   },
   editExam: async (req,res)=>{
-    console.log("--------hitting route")
-      console.log(req.body.age)
     try{
       const exam = await Exam.findOneAndUpdate({_id: req.body._id},
         {
@@ -102,18 +100,20 @@ module.exports = {
     }
   },
   deleteExam: async (req, res) => {
-    console.log("deleteing")
-  //   try {
-  //     // Find post by id
-  //     let exam = await Exam.findById({ _id: req.params.id });
-  //     // Delete image from cloudinary
-  //     await cloudinary.uploader.destroy(exam.cloudinaryId);
-  //     // Delete post from db
-  //     await Exam.deleteOne({ _id: req.params.id });
-  //     console.log("Deleted Exam");
-  //     res.redirect("/profile");
-  //   } catch (err) {
-  //     res.redirect("/profile");
-  //   }
+    try {
+      // Find post by id
+      const exam = await Exam.findById({ _id: req.params.id });
+      // Delete image from cloudinary
+      if(exam.cloudinaryId){
+        await cloudinary.uploader.destroy(exam.cloudinaryId);
+      }
+      // Delete post from db
+      await Exam.deleteOne({ _id: req.params.id });
+      console.log("Deleted Exam");
+      res.json(exam);
+    } catch (err) {
+      console.log(err)
+      res.json({err:err})
+    }
   },
 };
