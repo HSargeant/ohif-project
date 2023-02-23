05507f7cac0ac9b96f82f213a8f3d3550b7bfbfb
hsargeant <hssoftwareengineering@gmail.com>
2023-02-23 12:13:49 -0500

    moved font awesome to html file

--- client/src/pages/Exams.js
+++ client/src/pages/Exams.js
@@ -45,10 +45,7 @@ export default function Exams() {
 
     <div>
       <div>
-        {/* <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-        /> */}
+
         {/* <form className="example" action="action_page.php">
           <input
             type="text"
@@ -58,6 +55,7 @@ export default function Exams() {
           />
           <button type="submit" onClick={filterData()}>
             <FaSearch/>
+            <i className="fa-fasearch></i>"
           </button>
         </form> */}
       </div>
