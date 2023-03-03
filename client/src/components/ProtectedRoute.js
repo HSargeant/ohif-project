469c031836a963438d91e54bdaa54ae835f2563a
hsargeant <hssoftwareengineering@gmail.com>
2023-03-02 21:26:08 -0500

    protected route

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -3,8 +3,6 @@ import { useNavigate,useOutletContext, Navigate } from 'react-router-dom';
 import { API_BASE } from '../constants';
 
 const ProtectedRoute = ({children}) => {
-  console.log(children,"Adsfasdfasdf")
-  const navigate = useNavigate();
   const {user,setUser} = useOutletContext()
   return user ? children : <Navigate to={"/"}/>
 };
