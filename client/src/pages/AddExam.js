373ab775fc1d7b8affabce81728458430f21137b
Harnaljia <harnaljia.fenelon@tufts.edu>
2023-03-04 10:34:24 -0500

    organized some code
    - redoing pull request

--- client/src/pages/AddExam.js
+++ client/src/pages/AddExam.js
@@ -137,8 +137,10 @@ export default function AddExam() {
             <label> X-ray Image </label>
             <input accept="image/*" type="file" name="file" />
           </div>
-
-          <input id="createButt" type="submit" value="Create Exam" />
+          <div className="SubmitButton">
+              <input id="createButt" type="submit" value="Create Exam" />
+          </div>
+          
         </form>
       </div>
     </div>
