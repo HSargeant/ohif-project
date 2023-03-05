b15808cc72050307d8624791b2643482c74cee90
Harnaljia <harnaljia.fenelon@tufts.edu>
2023-03-05 00:23:51 -0500

    Revert "Update ProtectedRoute.js"
    This reverts commit 12928c32a2479d32852594337b0dae8f80d43c62.

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -3,12 +3,7 @@ import { useNavigate,useOutletContext, Navigate } from 'react-router-dom';
 import { API_BASE } from '../constants';
 
 const ProtectedRoute = ({children}) => {
-<<<<<<< HEAD
   const navigate=useNavigate()
-=======
-  console.log(children,"Adsfasdfasdf")
-  const navigate = useNavigate();
->>>>>>> parent of 469c031 (protected route)
   const {user,setUser} = useOutletContext()
   return user ? children : navigate("/")
 };
