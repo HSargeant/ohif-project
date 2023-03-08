53d7225ed60e7f7e09d573b4c917e25c7df33731
hsargeant <hssoftwareengineering@gmail.com>
2023-03-08 13:50:09 -0500

    add file name next to upload image button

--- client/src/pages/AddExam.js
+++ client/src/pages/AddExam.js
@@ -1,146 +1,6 @@
 // // page to add new exam
 
-// import { API_BASE } from "../constants";
-// import { useState } from "react";
-// import { Link, useNavigate, useOutletContext,useLocation,Navigate} from "react-router-dom";
-// import NavBarSide from "../components/NavBarSide/navbarside";
-// export default function AddExam() {
-//     const navigate = useNavigate();
-//     const location =useLocation()
-//     const { user, setMessages } = useOutletContext();
-//     let handleSubmit = async (event) => {
-//     try {
-//       event.preventDefault();
-//       const form = event.currentTarget;
-//       const response = await fetch(API_BASE + form.getAttribute("action"), {
-//         method: form.method,
-//         body: new FormData(form),
-//         credentials: "include",
-//       });
-//       const data = await response.json();
-//     } catch (err) {
-//       console.log("Error:" + err);
-//     }
-//     // if (data.messages) setMessages(data.messages);
-//     navigate(-1);
-//   };
-//   // console.log(message);
-
-//   return (
-//     <div>
-//       <div className="ExamForm">
-//         <h1 style={{textAlign:"center"}}> Create a New Exam </h1>
-//         <form
-//           className="csForm"
-//           action="/api/exams/new"
-//           encType="multipart/form-data"
-//           method="POST"
-//           onSubmit={handleSubmit}
-//         >
-//           <div className="formFunction">
-//             <input type="reset" placeholder="RESET" />
-
-//             {location.key !== "default" && (
-//     <button onClick={() => navigate(-1)} type="button">Back</button>
-//   )}
-//           </div>
-
-//           <div className="row1">
-//             {/* <div id="rowCell"> */}
-//             <label> Patient ID</label>
-//             <input type="text" placeholder="Patient ID..." name="patientId" />
-//             {/* </div> */}
-//             {/* <div id="rowCell"> */}
-//             <label> Exam ID</label>
-//             <input type="text" placeholder="Exam ID..." name="examId" />
-//             {/* </div> */}
-//           </div>
-
-//           <div className="row2">
-//             <div id="rowCell">
-//               <label> Patient's Age</label>
-//               <input
-//                 type="number"
-//                 placeholder="Age"
-//                 min="0"
-//                 max="112"
-//                 name="age"
-//               />
-
-//               <label> Patient's Sex </label>
-//               <select name="sex">
-//                 <option disabled selected>
-//                   Male or Female
-//                 </option>
-//                 <option value="M">M</option>
-//                 <option value="F">F</option>
-//               </select>
-//             </div>
-
-//             <div id="rowCell">
-//               <label> Patient's ZipCode </label>
-//               <input type="text" placeholder="zip code" name="zipCode" />
-//             </div>
-//           </div>
-
-//           <div className="row3">
-//             <label> Patient's Weight</label>
-//             <input type="text" placeholder="Weight" name="weight" />
-
-//             <label> BMI</label>
-//             <input type="text" placeholder="BMI..." name="bmi" />
-//             <label> Brixia Score</label>
-//             <input
-//               type="text"
-//               placeholder="Brixia Score..."
-//               name="brixiaScores"
-//             />
-//           </div>
-
-//           <div>
-//             <label> icu </label>
-//             <select name="icu">
-//               <option disabled selected>
-//                 Y/N
-//               </option>
-//               <option value="Y">Y</option>
-//               <option value="N">N</option>
-//             </select>
-
-//             <label> icu admits </label>
-//             <input type="text" name="icuAdmits" />
-
-//             <label> mortality </label>
-//             <select name="mortality">
-//               <option disabled selected>
-//                 Y/N
-//               </option>
-//               <option value="Y">Y</option>
-//               <option value="N">N</option>
-//             </select>
-//           </div>
-
-//           <div className="row4">
-//             <label> Key Findings </label>
-//             <textarea
-//               placeholder="Please note any key findings"
-//               name="keyFindings"
-//             />
-//           </div>
-
-//           <div className="row5">
-//             <label> X-ray Image </label>
-//             <input accept="image/*" type="file" name="file" />
-//           </div>
-
-//           <input id="createButt" type="submit" value="Create Exam" />
-//         </form>
-//       </div>
-//     </div>
-//   );
-// }
-
-import { Link, useNavigate, useOutletContext,useLocation,Navigate} from "react-router-dom";
+import { Link, useNavigate, useOutletContext,useLocation } from "react-router-dom";
 import { useState } from 'react';
 import {
   Box,
@@ -151,6 +11,8 @@ import {
   Divider,
   Grid,
   TextField,
+  Input,
+  Typography
 } from '@mui/material';
 import { API_BASE } from "../constants";
 
@@ -195,6 +57,11 @@ export default function AddExam(props){
         }
         // if (data.messages) setMessages(data.messages);
         navigate(-1);
+      };
+      const [file, setFile] = useState(null);
+      const handleFileChange = (event) => {
+        console.log(event.target.files[0].name)
+        setFile(event.target.files[0].name);
       };
 
   return (
@@ -437,12 +304,14 @@ export default function AddExam(props){
                         >
                           Upload File
                           <input
+                            onChange={handleFileChange}
                             type="file"
                             hidden
                             accept="image/*"
                             name="file"
                           />
-                        </Button>
+                        </Button> 
+                        {file&&<span style={{marginLeft:10}}>{file}</span>}
                     </Grid>
                 </Grid>
                 </CardContent>
