c526e982adede939ed9709a57780ab8a41e44e7c
hsargeant <hssoftwareengineering@gmail.com>
2023-03-10 01:02:54 -0500

    add render shortcut

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE = process.env.NODE_ENV =='production' ? "https://team4-techdive.onrender.com/" :'http://localhost:8000'
+export const API_BASE = process.env.NODE_ENV =='production' ? "https://team4-techdive.onrender.com" :'http://localhost:8000'
 // = process.env.NODE_ENV =='production' ? "https://ohif-project-production.up.railway.app" :'http://localhost:8000'
 
 
