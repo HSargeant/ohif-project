2d4b37c04dd0a23689f33f843ff6e17a03490b3a
hsargeant <hssoftwareengineering@gmail.com>
2023-03-10 09:48:15 -0500

    update build

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,5 @@
-export const API_BASE="https://reactors-techdive.onrender.com"
+export const API_BASE = process.env.NODE_ENV =='production' ? "https://reactors-techdive.onrender.com" :'http://localhost:8000'
+
 
 console.log(process.env.NODE_ENV)
 // export const API_BASE = process.env.NODE_ENV =='production' ? "https://ohif-project-production.up.railway.app" :'http://localhost:8000'
