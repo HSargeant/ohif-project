824e3c6a6d5b0544c530b9a43a4e9fffc5a60a66
hsargeant <hssoftwareengineering@gmail.com>
2023-03-10 09:12:45 -0500

    render fix

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,5 +1,5 @@
-export const API_BASE = process.env.NODE_ENV =='production' ? "https://reactors-techdive.up.railway.app" :'http://localhost:8000'
+export const API_BASE = process.env.NODE_ENV =='production' ? "https://reactors-techdive.onrender.com" :'http://localhost:8000'
 
-
+console.log(process.env.NODE_ENV)
 // export const API_BASE = process.env.NODE_ENV =='production' ? "https://ohif-project-production.up.railway.app" :'http://localhost:8000'
-// // export const API_BASE = process.env.NODE_ENV =='production' ? "https://team4-techdive.onrender.com" :'http://localhost:8000'
+// // export const API_BASE = process.env.NODE_ENV =='production' ? "https://reactors-techdive.onrender.com" :'http://localhost:8000'
