6e654d13e3dc70cc53b53b58ddb2e03155f60ac2
hsargeant <hssoftwareengineering@gmail.com>
2023-03-10 09:29:57 -0500

    api base hard code

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE = process.env.NODE_ENV =='production' ? "https://reactors-techdive.onrender.com" :'http://localhost:8000'
+export const API_BASE="https://reactors-techdive.onrender.com"
 
 console.log(process.env.NODE_ENV)
 // export const API_BASE = process.env.NODE_ENV =='production' ? "https://ohif-project-production.up.railway.app" :'http://localhost:8000'
